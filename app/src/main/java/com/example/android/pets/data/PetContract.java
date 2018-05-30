1c6136831959e5f24df41eac577d8f88a72b7909
GuieMigi <florinchiriac2004@yahoo.com>
2018-05-30 21:22:53 +0300

    Pets app at the end of lesson 4.10

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,10 +1,24 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
 
+    // The content authority is a name for the entire content provider.
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    // The base content uri that contains the scheme and the content authority.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    // The path for the pets table.
+    public static final String PATH_PETS = "pets";
+
+    private PetContract() {
+    }
+
     public static final class PetEntry implements BaseColumns {
+
+        // The content URI to access the pet data in the provider.
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
         // Table name.
         public static final String TABLE_NAME = "pets";
         // The _Id column.
